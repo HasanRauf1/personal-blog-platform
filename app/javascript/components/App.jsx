f1cafd689e29979eb7038e67be2c23b4baaf2eb4
Hasan Rauf <HasanR@winmo.com>
2023-11-21 23:53:50 -0600

    Transition to jsbundling-rails with React setup

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,14 @@
+// app/javascript/components/App.jsx
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My React Blog!</h1>
+      {/* Rest of your app goes here */}
+    </div>
+  );
+};
+
+export default App;
