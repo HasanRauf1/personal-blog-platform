5b5871c6c42a13c891b396ead67a70a76ce083cd
Hasan Rauf <HasanR@winmo.com>
2023-11-25 19:42:22 -0600

    Changes Introduced: React Structure Adjustment: Updated the application's JavaScript entry point to import the App component directly from the root of the javascript directory, streamlining the component structure. Removal of Stimulus Controllers: Deleted Stimulus controller files (hello_controller.js, application.js, and index.js in controllers directory), indicating a shift away from using Stimulus in the application. Model Associations Update: Removed the belongs_to :user association from both Post and Comment models. This change aligns with the new application structure where posts and comments are no longer tied to specific users. View and Route Adjustments: Removed the content from home/index.html.erb, simplifying the home view. Enhanced routing in routes.rb to include namespaced API routes for posts and comments, as well as a catch-all route for single-page application (SPA) behavior. Database Schema Update: Aligned the database schema with model changes by removing user_id references from posts and comments tables. Reason for Changes: These changes are part of an effort to simplify the application's structure and focus on core functionalities of posting and commenting. By removing user associations and Stimulus controllers, the application is streamlined for easier management and future development. The React application entry point is reorganized for a more intuitive component structure, and the routing changes lay the groundwork for a more effective SPA experience.

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_21_202956) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_24_061519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,6 +42,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_21_202956) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "posts", "users"
 end
